2e9d2e2065a27a9816988d7e82e55afdfd25909e
Mateusz Palyz <m.palyz@pilot.co>
2015-05-14 23:06:03 +0200

    add github as a provider

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["CONSUMER_KEY"], ENV["CONSUMER_SECRET"]
+  provider :github,  ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"]
 end
 
 OmniAuth.config.logger = Rails.logger
